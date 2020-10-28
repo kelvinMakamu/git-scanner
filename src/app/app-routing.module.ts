6cfa474620c0062918f2d1d9afb4da5ed3bab9a1
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-10-28 20:39:57 +0300

    Modifies app-routing module file to include users component child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsersComponent } from './components/users/users.component';
+import { RepoComponent } from './components/repo/repo.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'users', component: UsersComponent },
+  { path: 'users', component: UsersComponent, children: [
+    { path: ':id', component: RepoComponent },
+  ] },
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
