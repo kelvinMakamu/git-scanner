0bf94172d458cd0b8a8a6cb1fc583798f109f16d
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-10-29 03:48:22 +0300

    Adds project configurations

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+import { COLORS } from './config';
+
+export const environment = {
+  production: true,
+  LANGUAGE_COLORS: COLORS ,
+  API_URL:'https://api.github.com',
+  API_KEY: 'aa6c0de1483c69bacb2acb19c9b16913ccfade42',
+  USER: 'kelvinMakamu'
+};
