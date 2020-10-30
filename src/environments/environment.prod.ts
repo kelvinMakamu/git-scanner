77d6e670c539b72cb253d3721b583bea4eaba36e
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-10-30 21:55:41 +0300

    Fixed gitignore misbehaving

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-import { COLORS } from './config';
-
-export const environment = {
-  production: true,
-  LANGUAGE_COLORS: COLORS ,
-  API_URL:'https://api.github.com',
-  API_KEY: 'aa6c0de1483c69bacb2acb19c9b16913ccfade42',
-  USER: 'kelvinMakamu'
-};
